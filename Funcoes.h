66e39d2fe5dcc519655b079a463912c93d38be0f
Rodrigo Rodrigues <rodstudart@gmail.com>
2017-06-27 17:43:57 -0300

    botando menu pra int

--- Funcoes.h
+++ Funcoes.h
@@ -7,5 +7,5 @@ void createNPC(NPC *p,int x,int y,int w,int h,int velX,int velY,char path[]);
 void moveNPC(NPC *p);
 void createBricks(int lvl);
 int  trackCollision(NPC *p,int opt);
-void menu();
+int menu();
 #endif
